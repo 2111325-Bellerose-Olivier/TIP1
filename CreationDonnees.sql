15d28f519c1abcea85233dfbdd5e026129119b39
2143781-Ouellette-Antoine <2143781@etudiant.cegepvicto.ca>
2022-02-22 13:47:58 -0500

    CréationDonnees.sql complet et fonctionnel; Les create sont faits.

--- CreationDonnees.sql
+++ CreationDonnees.sql
@@ -1,8 +1,7 @@
 /*
- * Code de création de la bd du Planet Express et d'insertion des données de commandes. 
- * Utilisé pour l'atelier 6 et les suivants
+ * Code de création des données de la bd pour Donjon Inc.
  *
- * Fichier : planet_express.sql
+ * Fichier : CreationDonnees.sql
  * Auteur : Olivier Belrose et Antoine Ouellette
  * Langage : SQL
  * Date : février 2022
@@ -14,29 +13,12 @@ USE RessourcesMonstrueuses;
 
 CREATE TABLE Salle (
 	id_salle INTEGER PRIMARY KEY AUTO_INCREMENT,
-    fonction VARCHAR(255),
-    longueur FLOAT,
-    largeur FLOAT,
-    salle_suivante INTEGER,
-    /* Contraintes */
-    FOREIGN KEY (salle_suivante) REFERENCES Salle(id_salle) # fleche salle_suivante vers id_salle
-);
-
-CREATE TABLE Visite_salle (
-	salle INTEGER,
-    expedition INTEGER,
-    moment_visite DATETIME,
-    appreciation TEXT,
-    /* Contraintes */
-    PRIMARY KEY (salle, expedition),
-    FOREIGN KEY (salle) REFERENCES Salle(id_salle) # fleche salle vers id_salle
-);
-
-CREATE TABLE Expedition (
-	id_expedition INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom_equipe VARCHAR(255),
-    depart DATETIME,
-    terminaison DATETIME
+	fonction VARCHAR(255),
+	longueur FLOAT,
+	largeur FLOAT,
+	salle_suivante INTEGER,
+	/* Contraintes */
+	FOREIGN KEY (salle_suivante) REFERENCES Salle(id_salle)
 );
 
 CREATE TABLE Aventurier (
@@ -48,13 +30,38 @@ CREATE TABLE Aventurier (
     attaque INTEGER
 );
 
+CREATE TABLE Expedition (
+	id_expedition INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom_equipe VARCHAR(255),
+    depart DATETIME,
+    terminaison DATETIME
+);
+
 CREATE TABLE Expedition_aventurier (
 	id_expedition INTEGER,
     id_aventurier INTEGER,
     /* Contraintes */
     PRIMARY KEY (id_expedition, id_aventurier),
-    FOREIGN KEY (id_expedition) REFERENCES Expedition(id_expedition), # fleche id_expedition vers id_expedition
-    FOREIGN KEY (id_aventurier) REFERENCES Aventurier(id_aventurier) # fleche id_aventurier vers id_aventurier
+    FOREIGN KEY (id_expedition) REFERENCES Expedition(id_expedition),
+    FOREIGN KEY (id_aventurier) REFERENCES Aventurier(id_aventurier)
+);
+
+CREATE TABLE Visite_salle (
+	salle INTEGER,
+    expedition INTEGER,
+    moment_visite DATETIME,
+    appreciation TEXT,
+    /* Contraintes */
+    PRIMARY KEY (salle, expedition),
+    FOREIGN KEY (salle) REFERENCES Salle(id_salle),
+    FOREIGN KEY (expedition) REFERENCES Expedition(id_expedition)
+);
+
+CREATE TABLE Objet (
+	id_objet INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255),
+    valeur INT,
+    masse FLOAT
 );
 
 CREATE TABLE Coffre_tresor (
@@ -62,7 +69,7 @@ CREATE TABLE Coffre_tresor (
 	code_secret CHAR(64),
     salle INTEGER,
     /* Contraintes */
-    FOREIGN KEY (salle) REFERENCES Salle(id_salle) # fleche salle vers id_salle
+    FOREIGN KEY (salle) REFERENCES Salle(id_salle)
 );
 
 CREATE TABLE Ligne_coffre (
@@ -71,14 +78,61 @@ CREATE TABLE Ligne_coffre (
     quantite INTEGER,
     /* Contraintes */
     PRIMARY KEY (coffre, objet),
-    FOREIGN KEY (coffre) REFERENCES Coffre_tresor(id_coffre_tresor) # fleche coffre vers id_coffre_tresor
+    FOREIGN KEY (coffre) REFERENCES Coffre_tresor(id_coffre_tresor)
 );
 
-CREATE TABLE Objet (
-	id_objet INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(255),
-    valeur INT,
-    masse FLOAT
+CREATE TABLE Famille_monstre (
+	id_famille INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom_famille VARCHAR(255),
+    point_vie_maximal INTEGER,
+    degat_base INTEGER,
+    famille INTEGER
+);
+
+CREATE TABLE Humanoide (
+	id_humanoide INTEGER PRIMARY KEY AUTO_INCREMENT,
+    famille INTEGER,
+    arme VARCHAR(255),
+    intelligence INTEGER,
+    /* Contraintes */
+    FOREIGN KEY (famille) REFERENCES Famille_monstre(id_famille)
+);
+
+CREATE TABLE Mort_vivant (
+	id_mort_vivant INTEGER PRIMARY KEY AUTO_INCREMENT,
+    famille INTEGER,
+    vulnerable_soleil TINYINT,
+    infectieux TINYINT,
+    /* Contraintes */
+    FOREIGN KEY (famille) REFERENCES Famille_monstre(id_famille)
+);
+
+CREATE TABLE Elementaire (
+	id_elementaire INTEGER PRIMARY KEY AUTO_INCREMENT,
+    famille INTEGER,
+    element ENUM('Feu', 'Eau', 'Terre', 'Air'),
+    taille ENUM('Petit', 'Moyen', 'Gros'),
+    /* Contraintes */
+    FOREIGN KEY (famille) REFERENCES Famille_monstre(id_famille)
+);
+
+CREATE TABLE Monstre (
+	id_monstre INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nom VARCHAR(255),
+	code_employe CHAR(4),
+	point_vie INTEGER,
+	attaque INTEGER,
+	numero_ass_maladie BLOB,
+	id_famille INTEGER,
+	experience INTEGER,
+	/* Contraintes */
+	FOREIGN KEY (id_famille) REFERENCES Famille_monstre(id_famille)
+);
+
+CREATE TABLE Responsabilite (
+	id_responsabilite INTEGER PRIMARY KEY AUTO_INCREMENT,
+	titre VARCHAR(255),
+	niveau_responsabilite INTEGER
 );
 
 CREATE TABLE Affectation_salle (
@@ -89,28 +143,7 @@ CREATE TABLE Affectation_salle (
     fin_affectation DATETIME,
     /* Contraintes */
     PRIMARY KEY (monstre, salle),
-    FOREIGN KEY (salle) REFERENCES Salle(id_salle), # fleche salle vers id_salle
-    FOREIGN KEY (responsabilite) REFERENCES Responsabilite(id_responsabilite) # fleche responsabilite vers id_responsabilite
+    FOREIGN KEY (monstre) REFERENCES Monstre(id_monstre),
+    FOREIGN KEY (responsabilite) REFERENCES Responsabilite(id_responsabilite),
+    FOREIGN KEY (salle) REFERENCES Salle(id_salle)
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
