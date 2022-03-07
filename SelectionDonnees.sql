952e47c9267f20ed6e4f131ae7ac3b278da1f458
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2022-03-07 11:48:06 -0500

    Completer Question B

--- SelectionDonnees.sql
+++ SelectionDonnees.sql
@@ -8,7 +8,13 @@
  */
  
 USE RessourcesMonstrueuses;
- 
+
+# Question A
 SELECT nom, numero_ass_maladie FROM Monstre
 NATURAL JOIN Famille_monstre
 INNER JOIN Humanoide ON id_famille = famille;
+
+# Question B
+SELECT id_salle, longueur*largeur AS aire FROM Salle
+	ORDER by longueur*largeur DESC
+	LIMIT 1;
